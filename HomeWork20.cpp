9a732c9d8a398b02d2043fb2de496b3c4931a678
allrubtsoff <allrubtsooff@yandex.ru>
2021-08-25 12:44:07 +0300

    Перечисления

--- HomeWork20.cpp
+++ HomeWork20.cpp
@@ -1,7 +1,45 @@
 ﻿#include <iostream>
 
+enum class Result
+{
+    SUCCESS,
+    FAIL,
+    ERROR_WRITE,
+    ERROR_READ
+};
+
+enum class Another
+{
+    WORK,
+    SUCCESS
+};
+
+Result DoWork()
+{
+    return Result::ERROR_WRITE;
+}
+
+enum Alphabet
+{
+    A,
+    B,
+    C
+};
+
 int main()
 {
-    
+    Result workResult = DoWork();
+    std::cout << static_cast<int>(workResult);
+    static_cast<Result>(3);
+    if (workResult == Result::SUCCESS)
+    {
+        
+    }
+    Another anotherRes = Another::WORK;
+    //if (workResult == anotherRes)
+    {
+
+    }
+
 }
 
